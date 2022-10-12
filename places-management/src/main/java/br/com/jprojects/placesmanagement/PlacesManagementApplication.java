38ae0289ee8f825bce8b5f1de1fac93ec2367a11
Jean Divino <jeanpldivino@gmail.com>
2022-10-12 12:06:19 -0300

    Initial commit

---
+++ places-management/src/main/java/br/com/jprojects/placesmanagement/PlacesManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.jprojects.placesmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacesManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacesManagementApplication.class, args);
+	}
+
+}
